dd7d325aa0a4606c6e9c05f76aa3109e8f133ddb
yll14 <2443958507@qq.com>
2026-05-29 20:11:39 +0800

    修正错误的空间名

--- index.js
+++ index.js
@@ -177,8 +177,8 @@ logger.info(
     chalk.rgb(255, 165, 107)(`B`) +
     chalk.rgb(255, 231, 107)(`y`) +
     chalk.rgb(107, 255, 150)(` `) +
-    chalk.rgb(255, 107, 107)(global.XINGLUO.PluginAuthor[0]) +
-    chalk.rgb(255, 165, 107)(global.XINGLUO.PluginAuthor[1]),
+    chalk.rgb(255, 107, 107)(global.DailyHotPlugin.PluginAuthor[0]) +
+    chalk.rgb(255, 165, 107)(global.DailyHotPlugin.PluginAuthor[1]),
 );
 logger.info(
   chalk.rgb(255, 107, 107)(`-`) +
